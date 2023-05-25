c78e1647709442cd8a3fda14258efd51fec8e8e9
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-25 07:54:25 +0200

    likes and comments implementation

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,28 +1,21 @@
-import { getData, postData } from './api.js';
+import { postData } from './api.js';
 
 const microverseApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/YmJfq02jOUfGZEDWh0Nq/';
 
-export function fetchComments(show) {
-  getData(`${microverseApi}${show.title}`);
-}
-
-export function fetchLikes() {
-  getData(`${microverseApi}likes`);
-}
-
 export function postComment(show, uname, comment) {
   const data = {
-    item_id: show.title,
+    item_id: show.title.split(' ').join(''),
     username: uname,
     comment,
   };
 
+  // console.log(data);
   postData(`${microverseApi}comments`, data);
 }
 
 export function postLike(show) {
   const data = {
-    item_id: show.title,
+    item_id: show.title.split(' ').join(''),
   };
   postData(`${microverseApi}likes`, data);
 }
